62d7f0b92cb069b1075fd7b0220de5bf796c5cb4
Higor Pilon <higorp94@hotmail.com>
2021-02-10 18:35:27 +0000

    final commit

--- UmbracoV8/App_Data/Models/models.generated.cs
+++ UmbracoV8/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "ca72faacb9851422")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
